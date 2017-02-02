80fd1e1e6447b935c7230a13f133a169cc545cf9
Anthony Krivonos <abkrivonos@gmail.com>
2017-02-02 04:33:12 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+//     next();
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('currentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+})
+
+app.get('/', (req, res) => {
+    res.render('index.hbs', {
+        welcomeMessage: 'Welcome to my website!',
+        pageTitle: 'Home Page'
+    })
+    // res.send('<h1>Hello Express!</h1>');
+    // res.send({
+    //     name: 'Anthony',
+    //     likes: [
+    //         'Basketball',
+    //         'Photography',
+    //         'Coding'
+    //     ]
+    // })
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+    // res.send('About Page');
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        error: 'Unable to handle request'
+    })
+});
+
+app.listen(8081, () => {
+    console.log('Server is up on port 8081.');
+});
